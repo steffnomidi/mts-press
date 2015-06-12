8c7f974b0279c7d3dab92f5cd3f53d56ec120ae4
Steffnomidi <steffnomidi@gmail.com>
2015-06-12 13:38:52 +0500

    add screen Polzovatelskoe soglashenie
    add 3.php
    add number for favorite in header

---
+++ mts-press/3.php
@@ -0,0 +1,94 @@
+<?php include "header.html"; ?>
+
+	<section>
+		 <div class="container" id="journal-number" >
+			<div class="row clearfix">
+				<div class="col-sm-4 image">
+						<img src="images/mag_big.png" title="" />
+				</div>
+				<div class="col-sm-5">
+					<div class="row-fluid top titledate">
+						<div class="title">Title <div class="date">Май 2015</div></div>
+					</div>
+					<div class="row-fluid bottom favorite visible-xs-block">
+						<a href="#" class="btn btn-link">
+							<i class="icon icon icon-favor"></i>В избранное
+						</a>
+					</div>
+					<div class="row-fluid bottom">
+						<div class="subscribebutton">
+							<button class="subscribe danger">Читать</button>
+							<br />&nbsp
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-3 favoritsocial">
+					<div class="row-fluid bottom favorite hidden-xs">
+						<button class="favor favor-close">
+							<i class="icon icon icon-favor"></i>В избранном &nbsp;<a href=""><span class="close close-red"></span></a>
+						</button>
+						<br />&nbsp
+					</div>
+					<div class="row-fluid top social">
+						<span class="hidden-xs">Поделиться:</span>
+						<a href="#"><img src="images/fb_big.png"></a>
+						<a href="#"><img src="images/vk_big.png"></a>
+					</div>
+				</div>
+			</div>
+		 </div>
+	</section>
+	
+	<section id="pop" class="main">
+		<div class="container">
+			<h2>Другие номера</h2>
+			<div class="scroller">
+			<ul class="list">
+			
+				<li class="journal col-md-3 col-sm-4 col-xs-12">
+					<a href="#" title ""><img src="images/mag_min.png" title="" /> <img src="" title="" />
+						<h3 class="title">Title <div class="date">№5, 3 Мая 2015</div></h3>
+					</a>
+				</li>
+				<li class="journal col-md-3 col-sm-4 col-xs-12">
+					<a href="#" title ""><img src="images/mag_min.png" title="" /> <img src="" title="" />
+						<h3 class="title">Title <div class="date">№5, 3 Мая 2015</div></h3>
+					</a>
+				</li>
+				<li class="journal col-md-3 col-sm-4 col-xs-12">
+					<a href="#" title ""><img src="images/mag_min.png" title="" /> <img src="" title="" />
+						<h3 class="title">Title <div class="date">№5, 3 Мая 2015</div></h3>
+					</a>
+				</li>
+				<li class="journal col-md-3 col-sm-4 col-xs-12">
+					<a href="#" title ""><img src="images/mag_min.png" title="" /> <img src="" title="" />
+						<h3 class="title">Title <div class="date">№5, 3 Мая 2015</div></h3>
+					</a>
+				</li>
+				<li class="journal col-md-3 col-sm-4 col-xs-12">
+					<a href="#" title ""><img src="images/mag_min.png" title="" /> <img src="" title="" />
+						<h3 class="title">Title <div class="date">№5, 3 Мая 2015</div></h3>
+					</a>
+				</li>
+				<li class="journal col-md-3 col-sm-4 col-xs-12">
+					<a href="#" title ""><img src="images/mag_min.png" title="" /> <img src="" title="" />
+						<h3 class="title">Title <div class="date">№5, 3 Мая 2015</div></h3>
+					</a>
+				</li>
+				<li class="journal col-md-3 col-sm-4 col-xs-12">
+					<a href="#" title ""><img src="images/mag_min.png" title="" /> <img src="" title="" />
+						<h3 class="title">Title <div class="date">№5, 3 Мая 2015</div></h3>
+					</a>
+				</li>
+				<li class="journal col-md-3 col-sm-4 col-xs-12">
+					<a href="#" title ""><img src="images/mag_min.png" title="" /> <img src="" title="" />
+						<h3 class="title">Title <div class="date">№5, 3 Мая 2015</div></h3>
+					</a>
+				</li>
+				
+			</ul>
+			</div>
+		</div>
+	</section>
+
+<?php include "footer.html"; ?>
