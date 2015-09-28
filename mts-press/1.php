9eb943921946aa6a9badd441cd22de7cfdde09c1
Steffnomidi <steffnomidi@gmail.com>
2015-09-28 22:43:03 +0500

    final (?) commit

--- mts-press/1.php
+++ mts-press/1.php
@@ -1,161 +1,161 @@
-<?php include "header.html"; ?>
-
-	<section id="j_slider">
-
-		<ul class="slider">
-		
-			<li class="journal">
-				<a href="#" title=""><img alt="" src="images/mag_big.png" title="" />
-					<span class="title">Title <span class="date">Май 2015</span></span>
-				</a>
-			</li>
-			<li class="journal">
-				<a href="#" title=""><img alt="" src="images/mag_big.png" title="" />
-					<span class="title">Title <span class="date">Май 2015</span></span>
-				</a>
-			</li>
-			<li class="journal">
-				<a href="#" title=""><img alt="" src="images/mag_big.png" title="" />
-					<span class="title">Title <span class="date">Май 2015</span></span>
-				</a>
-			</li>
-			<li class="journal">
-				<a href="#" title=""><img alt="" src="images/mag_big.png" title="" />
-					<span class="title">Title <span class="date">Май 2015</span></span>
-				</a>
-			</li>
-			<li class="journal">
-				<a href="#" title=""><img alt="" src="images/mag_big.png" title="" />
-					<span class="title">Title <span class="date">Май 2015</span></span>
-				</a>
-			</li>
-			<li class="journal">
-				<a href="#" title=""><img alt="" src="images/mag_big.png" title="" />
-					<span class="title">Title <span class="date">Май 2015</span></span>
-				</a>
-			</li>
-			<li class="journal">
-				<a href="#" title=""><img alt="" src="images/mag_big.png" title="" />
-					<span class="title">Title <span class="date">Май 2015</span></span>
-				</a>
-			</li>
-			<li class="journal">
-				<a href="#" title=""><img alt="" src="images/mag_big.png" title="" />
-					<span class="title">Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title <span class="date">Май 2015</span></span>
-				</a>
-			</li>
-			<li class="journal">
-				<a href="#" title=""><img alt="" src="images/mag_big.png" title="" />
-					<span class="title">Title <span class="date">Май 2015</span></span>
-				</a>
-			</li>
-			<li class="journal">
-				<a href="#" title=""><img alt="" src="images/mag_big.png" title="" />
-					<span class="title">Title <span class="date">Май 2015</span></span>
-				</a>
-			</li>
-			<li class="journal">
-				<a href="#" title=""><img alt="" src="images/mag_big.png" title="" />
-					<span class="title">Title <span class="date">Май 2015</span></span>
-				</a>
-			</li>
-			
-		</ul>
-			
-	</section>
-	
-	<section id="pop" class="main">
-		<div class="container">
-			<div class="scroller">
-				<div>
-					<h2>Популярное</h2>
-					<ul class="list">
-					
-						<li class="journal col-md-3 col-sm-4 col-xs-12">
-							<a href="#" title=""><img alt="" src="images/mag_min.png" title="" />
-								<h3 class="title">Title <span class="date">№5, 3 Мая 2015</span></h3>
-							</a>
-						</li>
-						<li class="journal col-md-3 col-sm-4 col-xs-12">
-							<a href="#" title=""><img alt="" src="images/mag_min.png" title="" />
-								<span class="title">Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title <span class="date">№5, 3 Мая 2015</span></span>
-							</a>
-						</li>
-						<li class="journal col-md-3 col-sm-4 col-xs-12">
-							<a href="#" title=""><img alt="" src="images/mag_min.png" title="" />
-								<h3 class="title">Title <span class="date">№5, 3 Мая 2015</span></h3>
-							</a>
-						</li>
-						<li class="journal col-md-3 col-sm-4 col-xs-12">
-							<a href="#" title=""><img alt="" src="images/mag_min.png" title="" />
-								<h3 class="title">Title <span class="date">№5, 3 Мая 2015</span></h3>
-							</a>
-						</li>
-						<li class="journal col-md-3 col-sm-4 col-xs-12">
-							<a href="#" title=""><img alt="" src="images/mag_min.png" title="" />
-								<h3 class="title">Title <span class="date">№5, 3 Мая 2015</span></h3>
-							</a>
-						</li>
-						<li class="journal col-md-3 col-sm-4 col-xs-12">
-							<a href="#" title=""><img alt="" src="images/mag_min.png" title="" />
-								<h3 class="title">Title <span class="date">№5, 3 Мая 2015</span></h3>
-							</a>
-						</li>
-						<li class="journal col-md-3 col-sm-4 col-xs-12">
-							<a href="#" title=""><img alt="" src="images/mag_min.png" title="" />
-								<h3 class="title">Title <span class="date">№5, 3 Мая 2015</span></h3>
-							</a>
-						</li>
-						<li class="journal col-md-3 col-sm-4 col-xs-12">
-							<a href="#" title=""><img alt="" src="images/mag_min.png" title="" />
-								<h3 class="title">Title <span class="date">№5, 3 Мая 2015</span></h3>
-							</a>
-						</li>
-						<li class="journal col-md-3 col-sm-4 col-xs-12">
-							<a href="#" title=""><img alt="" src="images/mag_min.png" title="" />
-								<h3 class="title">Title <span class="date">№5, 3 Мая 2015</span></h3>
-							</a>
-						</li>
-						<li class="journal col-md-3 col-sm-4 col-xs-12">
-							<a href="#" title=""><img alt="" src="images/mag_min.png" title="" />
-								<h3 class="title">Title <span class="date">№5, 3 Мая 2015</span></h3>
-							</a>
-						</li>
-						<li class="journal col-md-3 col-sm-4 col-xs-12">
-							<a href="#" title=""><img alt="" src="images/mag_min.png" title="" />
-								<h3 class="title">Title <span class="date">№5, 3 Мая 2015</span></h3>
-							</a>
-						</li>
-						<li class="journal col-md-3 col-sm-4 col-xs-12">
-							<a href="#" title=""><img alt="" src="images/mag_min.png" title="" />
-								<h3 class="title">Title <span class="date">№5, 3 Мая 2015</span></h3>
-							</a>
-						</li>
-						<li class="journal col-md-3 col-sm-4 col-xs-12">
-							<a href="#" title=""><img alt="" src="images/mag_min.png" title="" />
-								<h3 class="title">Title <span class="date">№5, 3 Мая 2015</span></h3>
-							</a>
-						</li>
-						<li class="journal col-md-3 col-sm-4 col-xs-12">
-							<a href="#" title=""><img alt="" src="images/mag_min.png" title="" />
-								<h3 class="title">Title <span class="date">№5, 3 Мая 2015</span></h3>
-							</a>
-						</li>
-						<li class="journal col-md-3 col-sm-4 col-xs-12">
-							<a href="#" title=""><img alt="" src="images/mag_min.png" title="" />
-								<h3 class="title">Title <span class="date">№5, 3 Мая 2015</span></h3>
-							</a>
-						</li>
-						<li class="journal col-md-3 col-sm-4 col-xs-12">
-							<a href="#" title=""><img alt="" src="images/mag_min.png" title="" />
-								<h3 class="title">Title <span class="date">№5, 3 Мая 2015</span></h3>
-							</a>
-						</li>
-						
-					</ul>
-				</div>
-			</div>
-		</div>
-	</section>
-
+<?php include "header.html"; ?>
+
+	<section id="j_slider">
+
+		<ul class="slider">
+		
+			<li class="journal">
+				<a href="#" title=""><img alt="" src="images/mag_big.png" title="" />
+					<span class="title">Title <span class="date">Май 2015</span></span>
+				</a>
+			</li>
+			<li class="journal">
+				<a href="#" title=""><img alt="" src="images/mag_big.png" title="" />
+					<span class="title">Title <span class="date">Май 2015</span></span>
+				</a>
+			</li>
+			<li class="journal">
+				<a href="#" title=""><img alt="" src="images/mag_big.png" title="" />
+					<span class="title">Title <span class="date">Май 2015</span></span>
+				</a>
+			</li>
+			<li class="journal">
+				<a href="#" title=""><img alt="" src="images/mag_big.png" title="" />
+					<span class="title">Title <span class="date">Май 2015</span></span>
+				</a>
+			</li>
+			<li class="journal">
+				<a href="#" title=""><img alt="" src="images/mag_big.png" title="" />
+					<span class="title">Title <span class="date">Май 2015</span></span>
+				</a>
+			</li>
+			<li class="journal">
+				<a href="#" title=""><img alt="" src="images/mag_big.png" title="" />
+					<span class="title">Title <span class="date">Май 2015</span></span>
+				</a>
+			</li>
+			<li class="journal">
+				<a href="#" title=""><img alt="" src="images/mag_big.png" title="" />
+					<span class="title">Title <span class="date">Май 2015</span></span>
+				</a>
+			</li>
+			<li class="journal">
+				<a href="#" title=""><img alt="" src="images/mag_big.png" title="" />
+					<span class="title">Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title <span class="date">Май 2015</span></span>
+				</a>
+			</li>
+			<li class="journal">
+				<a href="#" title=""><img alt="" src="images/mag_big.png" title="" />
+					<span class="title">Title <span class="date">Май 2015</span></span>
+				</a>
+			</li>
+			<li class="journal">
+				<a href="#" title=""><img alt="" src="images/mag_big.png" title="" />
+					<span class="title">Title <span class="date">Май 2015</span></span>
+				</a>
+			</li>
+			<li class="journal">
+				<a href="#" title=""><img alt="" src="images/mag_big.png" title="" />
+					<span class="title">Title <span class="date">Май 2015</span></span>
+				</a>
+			</li>
+			
+		</ul>
+			
+	</section>
+	
+	<section id="pop" class="main">
+		<div class="container">
+			<div class="scroller">
+				<div>
+					<h2>Популярное</h2>
+					<ul class="list">
+					
+						<li class="journal col-md-3 col-sm-4 col-xs-12">
+							<a href="#" title=""><img alt="" src="images/mag_min.png" title="" />
+								<h3 class="title">Title <span class="date">№5, 3 Мая 2015</span></h3>
+							</a>
+						</li>
+						<li class="journal col-md-3 col-sm-4 col-xs-12">
+							<a href="#" title=""><img alt="" src="images/mag_min.png" title="" />
+								<span class="title">Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title <span class="date">№5, 3 Мая 2015</span></span>
+							</a>
+						</li>
+						<li class="journal col-md-3 col-sm-4 col-xs-12">
+							<a href="#" title=""><img alt="" src="images/mag_min.png" title="" />
+								<h3 class="title">Title <span class="date">№5, 3 Мая 2015</span></h3>
+							</a>
+						</li>
+						<li class="journal col-md-3 col-sm-4 col-xs-12">
+							<a href="#" title=""><img alt="" src="images/mag_min.png" title="" />
+								<h3 class="title">Title <span class="date">№5, 3 Мая 2015</span></h3>
+							</a>
+						</li>
+						<li class="journal col-md-3 col-sm-4 col-xs-12">
+							<a href="#" title=""><img alt="" src="images/mag_min.png" title="" />
+								<h3 class="title">Title <span class="date">№5, 3 Мая 2015</span></h3>
+							</a>
+						</li>
+						<li class="journal col-md-3 col-sm-4 col-xs-12">
+							<a href="#" title=""><img alt="" src="images/mag_min.png" title="" />
+								<h3 class="title">Title <span class="date">№5, 3 Мая 2015</span></h3>
+							</a>
+						</li>
+						<li class="journal col-md-3 col-sm-4 col-xs-12">
+							<a href="#" title=""><img alt="" src="images/mag_min.png" title="" />
+								<h3 class="title">Title <span class="date">№5, 3 Мая 2015</span></h3>
+							</a>
+						</li>
+						<li class="journal col-md-3 col-sm-4 col-xs-12">
+							<a href="#" title=""><img alt="" src="images/mag_min.png" title="" />
+								<h3 class="title">Title <span class="date">№5, 3 Мая 2015</span></h3>
+							</a>
+						</li>
+						<li class="journal col-md-3 col-sm-4 col-xs-12">
+							<a href="#" title=""><img alt="" src="images/mag_min.png" title="" />
+								<h3 class="title">Title <span class="date">№5, 3 Мая 2015</span></h3>
+							</a>
+						</li>
+						<li class="journal col-md-3 col-sm-4 col-xs-12">
+							<a href="#" title=""><img alt="" src="images/mag_min.png" title="" />
+								<h3 class="title">Title <span class="date">№5, 3 Мая 2015</span></h3>
+							</a>
+						</li>
+						<li class="journal col-md-3 col-sm-4 col-xs-12">
+							<a href="#" title=""><img alt="" src="images/mag_min.png" title="" />
+								<h3 class="title">Title <span class="date">№5, 3 Мая 2015</span></h3>
+							</a>
+						</li>
+						<li class="journal col-md-3 col-sm-4 col-xs-12">
+							<a href="#" title=""><img alt="" src="images/mag_min.png" title="" />
+								<h3 class="title">Title <span class="date">№5, 3 Мая 2015</span></h3>
+							</a>
+						</li>
+						<li class="journal col-md-3 col-sm-4 col-xs-12">
+							<a href="#" title=""><img alt="" src="images/mag_min.png" title="" />
+								<h3 class="title">Title <span class="date">№5, 3 Мая 2015</span></h3>
+							</a>
+						</li>
+						<li class="journal col-md-3 col-sm-4 col-xs-12">
+							<a href="#" title=""><img alt="" src="images/mag_min.png" title="" />
+								<h3 class="title">Title <span class="date">№5, 3 Мая 2015</span></h3>
+							</a>
+						</li>
+						<li class="journal col-md-3 col-sm-4 col-xs-12">
+							<a href="#" title=""><img alt="" src="images/mag_min.png" title="" />
+								<h3 class="title">Title <span class="date">№5, 3 Мая 2015</span></h3>
+							</a>
+						</li>
+						<li class="journal col-md-3 col-sm-4 col-xs-12">
+							<a href="#" title=""><img alt="" src="images/mag_min.png" title="" />
+								<h3 class="title">Title <span class="date">№5, 3 Мая 2015</span></h3>
+							</a>
+						</li>
+						
+					</ul>
+				</div>
+			</div>
+		</div>
+	</section>
+
 <?php include "footer.html"; ?>
